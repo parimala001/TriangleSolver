174d2e10830be5bfab233f2f80a16db52274b4a0
parimala001 <ganthiparimala001@gmail.com>
2024-03-06 17:36:44 -0500

    Eqilateral Triangle test

--- TriangleTesting/Class1.cs
+++ TriangleTesting/Class1.cs
@@ -27,5 +27,52 @@ namespace TriangleTests
            ClassicAssert.AreEqual("Equilateral triangle", result);
         }
 
+        //for isosceles
+        [Test]
+        public void AnalyzeTriangle_ValidIsosceles1()
+        {
+            // Arrange
+            int side1 = 5;
+            int side2 = 5;
+            int side3 = 8;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual("Isosceles triangle", result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_ValidIsosceles2()
+        {
+            // Arrange
+            int side1 = 5;
+            int side2 = 8;
+            int side3 = 5;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            ClassicAssert.AreEqual("Isosceles triangle", result);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_ValidIsosceles3()
+        {
+            // Arrange
+            int side1 = 8;
+            int side2 = 5;
+            int side3 = 5;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+            
+            ClassicAssert.AreEqual("Isosceles triangle", result);
+        }
+
     }
 }
