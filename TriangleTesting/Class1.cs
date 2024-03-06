172a61144a04468b153f09c8d3735a0aeefaee76
parimala001 <ganthiparimala001@gmail.com>
2024-03-06 17:28:48 -0500

    Equilateral Triangle test

--- TriangleTesting/Class1.cs
+++ TriangleTesting/Class1.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-
+using NUnit.Framework.Legacy;
+using TriangleSolver;
 
 namespace TriangleTests
 {
-    public class Class1
+    [TestFixture]
+    public class TriangleTesting
     {
+        [Test]
+        public void AnalyzeTriangle_ValidEquilateral()
+        {
+            // Arrange
+            int side1 = 5;
+            int side2 = 5;
+            int side3 = 5;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(side1, side2, side3);
+
+            // Assert
+           ClassicAssert.AreEqual("Equilateral triangle", result);
+        }
+
     }
 }
